4557fa0144721177fe7e91daec905a9345c3cea5
dareddyupender <upenderreddy.d23@gmail.com>
2025-02-14 21:52:11 +0530

    home page controller new changes

---
+++ webapp/Homepage.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+  ], function (Controller, JSONModel) {
+    "use strict";
+  
+    return Controller.extend("your.namespace.controller.Main", {
+      onInit: function () {
+        // Create a JSON model with some sample data
+        var oModel = new JSONModel({
+          items: [
+            { name: "Item 1", description: "Description for Item 1" },
+            { name: "Item 2", description: "Description for Item 2" },
+            { name: "Item 3", description: "Description for Item 3" },
+            { name: "Item 4", description: "Description for Item 4" }
+          ]
+        });
+  
+        // Set the model to the view
+        this.getView().setModel(oModel);
+      }
+    });
+  });
+  
